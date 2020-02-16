09c5390132b6dcd2d54a48779e3ee761029bfed8
ben grad <bengrad26@gmail.com>
2020-02-16 08:50:35 +0200

    Add project files.

---
+++ BenFinalProject/BenFinalProject/views.py
@@ -0,0 +1,37 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from BenFinalProject import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact me',
+        year=datetime.now().year,
+        message='Ben Grad.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
